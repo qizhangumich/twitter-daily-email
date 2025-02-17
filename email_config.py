d2b22099c600a3f79a11b7c1fddd30a9a503218f
qizhangumich <93417893+qizhangumich@users.noreply.github.com>
2025-02-17 18:38:37 +0800

    Update email_config.py

--- email_config.py
+++ email_config.py
@@ -1,6 +1,5 @@
 # 📧 邮件接收者配置
 RECEIVER_EMAILS = [
     "zhangqi@cpe-fund.com",
-    "tonyshan05@yahoo.com",
-    "yuanmengyuan@cpe-fund.com"  # 你可以随时新增或删除
+    "sofiayuan@cpe-fund.com"  # 你可以随时新增或删除
 ]
